fa3c462a02ab7d4c365d665c1949278d07214b5b
SRISOWMITHRAPAWAN <rksmithru@gmail.com>
2023-01-06 17:07:33 +0530

    connection

--- connect.js
+++ connect.js
@@ -5,10 +5,11 @@ module.exports={
     selectedDb:{},
     async connect(){
         try{
-            const client=await MongoClient.connect(process.env.MONGODB_URL);
-            
+            const client=await MongoClient.connect(process.env.MONGODB_URL, {useNewUrlParser: true});
+           
             this.selectedDb=client.db("products");
-        console.log(this.selectedDb);
+        // console.log(this.selectedDb);
+        console.log("connection successfull")
         } catch(err){
             console.log(err);
         }
